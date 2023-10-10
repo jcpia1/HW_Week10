0276210a73143b986c53b773f9a9707fa2844386
jcpia1 <145896106+jcpia1@users.noreply.github.com>
2023-10-10 23:51:05 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
 var vg_1 = "HW10.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance
 (https://vega.github.io/veg
